657ece56a1cd3b7cf09415d7a997a6e10d5bc024
Yuki2dev <KeysHD@users.noreply.github.com>
2022-03-07 12:02:05 -0300

    📝 Reworked: Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,27 @@
 import type { GetServerSideProps, NextPage } from "next";
 import { usePageQuery, PageDocument } from "../generated/graphql";
 import { client, ssrCache } from "../libs/urql";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+import Pricing from "../components/Pricing";
 
-const Home: NextPage = () => {
+const HomePage: NextPage = () => {
   const [{ data }] = usePageQuery({
     variables: {
       slug: "about",
     },
   });
 
-  return <h1>{data?.page?.title}</h1>;
+  return (
+    <>
+      <Hero title={data?.page?.title} subtitle={data?.page?.subtitle} />
+      <Features />
+      <Pricing />
+    </>
+  );
 };
 
-export default Home;
+export default HomePage;
 
 export const getServerSideProps: GetServerSideProps = async () => {
   await client.query(PageDocument, { slug: "home" }).toPromise();
